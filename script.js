c58d3a83442f1fc14110a3fdeb8b7987e4e06814
sanmato <santiago.mato.rpc@gmail.com>
2024-05-16 22:44:40 -0300

    separado el arreglo de objetos en un JSON que se consume con fetch, se agrego una llamada asincrona para asegurar la carga

--- script.js
+++ script.js
@@ -1,80 +1,15 @@
-// Datos del menú. Pasados desde el HTML a un arreglo de objetos, con cada propiedad.
-const menuItems = [
-  {
-    category: "bebidas",
-    imgSrc: "ruta_de_la_imagen_espresso.jpg",
-    alt: "Café Espresso",
-    title: "Café Espresso",
-    description: "Un café fuerte y aromático servido en una taza pequeña.",
-  },
-  {
-    category: "bebidas",
-    imgSrc: "ruta_de_la_imagen_latte.jpg",
-    alt: "Café Latte",
-    title: "Café Latte",
-    description: "Una combinación suave de café espresso y leche caliente.",
-  },
-  {
-    category: "bakery",
-    imgSrc: "ruta_de_la_imagen_pastel.jpg",
-    alt: "Pastel de Chocolate",
-    title: "Pastel de Chocolate",
-    description: "Un delicioso pastel de chocolate para acompañar tu café.",
-  },
-  {
-    category: "bakery",
-    imgSrc: "ruta_de_la_imagen_pastel_matilda.jpg",
-    alt: "Bruce",
-    title: "Pastel de chocolate como Matilda!",
-    description:
-      "Una porción del icónico pastel de la icónica película para que también alientes a Bruce.",
-  },
-  {
-    category: "salados",
-    imgSrc: "ruta_de_croissant.jpg",
-    alt: "Croissant",
-    title: "Croissant Simple",
-    description:
-      "El más perfectamente horneado Croissant, para acompañar cualquier infusión!",
-  },
-  {
-    category: "cafe",
-    imgSrc: "ruta_de_cafe_colombia.jpg",
-    alt: "Colombia de Especialidad",
-    title: "Colombia de Especialidad",
-    description:
-      "Un café de intensidad moderada de especialidad, ideal para cafeteras Espresso o Prensa Francesa.",
-  },
-  {
-    category: "cafe",
-    imgSrc: "santos_bourbon.jpg",
-    alt: "SantosBourbon",
-    title: "Santos Bourbon",
-    description:
-      "De origen brasilero, de intensidad moderada pero un poco mas intensa que el Colombia, con un leve sabor a caramelo, ideal para cualquier cafetera.",
-  },
-  {
-    category: "cafe",
-    imgSrc: "familiar.jpg",
-    alt: "Familiar",
-    title: "Familiar",
-    description: "Café familiar en grano.",
-  },
-  {
-    category: "accesorios",
-    imgSrc: "cafetera_moka.jpg",
-    alt: "Moka",
-    title: "Cafetera Moka",
-    description: "Cafetera Moka de estilo italiano, capacidad de 6 pocillos.",
-  },
-  {
-    category: "accesorios",
-    imgSrc: "molinillo.jpg",
-    alt: "Molinillo",
-    title: "Molinillo Eléctrico",
-    description: "Molinillo eléctrico, con estilo de molienda a selección.",
-  },
-];
+const menuItems = [];
+
+const getMenuItems = () => {
+  return fetch("./items.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      menuItems.push(...json);
+    })
+    .catch((error) => console.error("Error al cargar el archivo JSON", error));
+};
 
 //Para obtener esto dinamicamente, es necesario cargar una serie de funciones.
 
@@ -115,6 +50,8 @@ const createMenuContainers = () => {
     createMenuItems(containerId, items);
   }
 };
+
+getMenuItems().then(createMenuContainers);
 
 window.addEventListener("DOMContentLoaded", () => {
   const navbarContainer = document.getElementById("navbar-container");
