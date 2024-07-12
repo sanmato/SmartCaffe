3f4e77f1e1e2de5acfa30a768e3dce373fc3d969
sanmato <santiago.mato.rpc@gmail.com>
2024-07-12 12:32:42 -0300

    users

--- models/user.js
+++ models/user.js
@@ -1,16 +1,27 @@
 const { DataTypes } = require("sequelize");
 const sequelize = require("../util/database");
 
-const User = sequelize.define("User", {
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
+const User = sequelize.define(
+  "User",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-});
+  {
+    tableName: "users", // Nombre de la tabla explícitamente especificado
+  }
+);
 
 module.exports = User;
